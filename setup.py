41d0f5e84c247fbc4dc296c6081791718b8c3c45
sahandilshan <sahandilshansg@gmail.com>
2021-07-11 13:39:42 +0530

    Modify setup.py configs

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="airML",
-    version="0.0.2",
+    version="0.0.3",
     author="Lahiru Oshara Hinguruduwa",
     author_email='oshara.16@cse.mrt.ac.lk',
     url='https://github.com/AKSW/airML',
@@ -19,10 +19,11 @@ setup(
     package_data={'': ['*.jar']},
     include_package_data=True,
     install_requires=['click>=7.1.2'],
-    entry_points='''
-        [console_scripts]
-        airML=airML.airML:execute_kbox_command
-    ''',
+    entry_points={
+        'console_scripts': [
+            'airML=airML.airML:execute_kbox_command',
+        ],
+    },
     license='Apache',
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
