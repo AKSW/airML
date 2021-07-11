7c394b2237a7e911928cd690f11320738be70b0a
sahandilshan <sahandilshansg@gmail.com>
2021-07-11 13:38:57 +0530

    Add support to share converted model into gofile.io

--- airML/airML.py
+++ airML/airML.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import requests
 
 import click
 
@@ -7,18 +8,36 @@ DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 JAR_EXECUTE = "java -jar " + DIR_PATH + "/kbox-v0.0.2-beta.jar"  # kbox-v0.0.2-beta.jar
 SPACE = " "
 
+PUSH_COMMAND = 'push'
+
+COMMAND_LIST = [PUSH_COMMAND]
+
 
 @click.command(context_settings={"ignore_unknown_options": True})
 @click.argument('commands', nargs=-1)
 def execute_kbox_command(commands):
-    args = SPACE
-    for item in commands:
-        args += item + SPACE
-    execute = JAR_EXECUTE + args
-    try:
-        process = subprocess.Popen(execute.split(), stdout=subprocess.PIPE)
-        output, err = process.communicate()
-        output = output.decode("utf-8")
-        click.echo(output)
-    except OSError as e:
-        click.echo(e)
+    if not __is_push(commands):
+        args = SPACE
+        for item in commands:
+            args += item + SPACE
+        execute = JAR_EXECUTE + args
+        try:
+            process = subprocess.Popen(execute.split(), stdout=subprocess.PIPE)
+            output, err = process.communicate()
+            output = output.decode("utf-8")
+            click.echo(output)
+        except OSError as e:
+            click.echo(e)
+    else:
+        file_path = commands[1]
+        print(file_path)
+        url = 'https://store9.gofile.io/uploadFile'
+        files = {'file': open(file_path, 'rb')}
+        r = requests.post(url, files=files)
+        print(r.text)
+
+
+def __is_push(command):
+    if command[0] == PUSH_COMMAND:
+        return True
+    return False
